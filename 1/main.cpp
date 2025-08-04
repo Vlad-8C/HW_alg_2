30abc90b5580dfafdd7e9f199abfb8f63f90c9c1
Vladislv <vlad.gritscsenko2w@gmail.com>
2025-08-04 20:51:25 +0500

    Add files via upload

---
+++ 1/main.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+
+int Fibon(int a, int b, int q)
+{
+    if (q == 0) return b;
+    int buf{ a + b };
+    Fibon(b, buf, q - 1);
+}
+
+
+int main()
+{
+    int a{ 1 }, b{ 2 }, Q{};
+    std::cout << "Enter quantyty number Fibonacci: \n"; std::cin >> Q;
+    std::cout << Fibon(a,b,Q-2) << "\n";
+    return EXIT_SUCCESS;
+}
+
